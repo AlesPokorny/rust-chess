01bca2b38e267bf336634fbcbb32fa96b567dd3e
Ales Pokorny <alda.pokec@gmail.com>
2024-05-11 22:59:31 +0200

    Add Position struct

--- src/moves.rs
+++ src/moves.rs
@@ -1,12 +1,5 @@
 use crate::positions::Position;
 
-fn is_position_on_board(position: &Position) -> bool {
-    if (position.x >= 0) & (position.x <= 7) & (position.y >= 0) & (position.y <= 7) {
-        return true
-    }
-    false
-}
-
 pub fn get_knight_moves(piece_position: &[usize; 2], friendly_positions: &Vec<Position>) -> Vec<Position> {
     let [x, y] = piece_position;
     let knight_moves: [(i32, i32); 8] = [
@@ -25,8 +18,7 @@ pub fn get_knight_moves(piece_position: &[usize; 2], friendly_positions: &Vec<Po
     for (move_x, move_y) in knight_moves {
         let new_x = *x as i32 + move_x;
         let new_y = *y as i32 + move_y;
-        let new_position = Position::new(new_x as usize, new_y as usize);
-        if is_position_on_board(&new_position) {
+        if let Some(new_position) = Position::get_valid_position(new_x, new_y){
             if !friendly_positions.contains(&new_position) {
                 moves.push(new_position);
             }
--- src/positions.rs
+++ src/positions.rs
@@ -9,4 +9,11 @@ impl Position {
     pub fn new(x: usize, y: usize) -> Position {
         Position { x, y }
     }
+
+    pub fn get_valid_position(x: i32, y: i32) -> Option<Position> {
+        if (x >= 0) & (x <= 7) & (y >= 0) & (y <= 7) {
+            return Some(Position::new(x as usize, y as usize))
+        }
+        None
+    }
 }
