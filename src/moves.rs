875b1916626aedfda37f6909f8a7da8dec27eb9a
Ales Pokorny <alda.pokec@gmail.com>
2024-05-11 22:59:31 +0200

    Add knight moves

--- src/moves.rs
+++ src/moves.rs
@@ -1,6 +1,6 @@
 
 
-pub fn get_knight_moves(piece_position: &[usize; 2], opponent_positions: &Vec<[usize; 2]>) -> Vec<(usize, usize)> {
+pub fn get_knight_moves(piece_position: &[usize; 2], friendly_positions: &Vec<[usize; 2]>) -> Vec<(usize, usize)> {
     let [x, y] = piece_position;
     let knight_moves: [(i32, i32); 8] = [
             (-2, -1),
@@ -21,7 +21,7 @@ pub fn get_knight_moves(piece_position: &[usize; 2], opponent_positions: &Vec<[u
         if (new_x >= 0) & (new_y >= 0) & (new_x <= 7) & (new_y <= 7) {
             let new_x = new_x as usize;
             let new_y = new_y as usize;
-            if !opponent_positions.contains(&[new_x, new_y]) {
+            if !friendly_positions.contains(&[new_x, new_y]) {
                 moves.push((new_x, new_y));
             }
         }
@@ -37,13 +37,13 @@ mod test_moves {
     #[test]
     fn test_get_knight_moves() {
         let piece_position = [1, 6];
-        let opponent_positions: Vec<[usize; 2]> = vec![
+        let friendly_positions: Vec<[usize; 2]> = vec![
             [0, 0],
             [2, 6],
             [3, 5],
         ];
 
-        let output = get_knight_moves(&piece_position, &opponent_positions);
+        let output = get_knight_moves(&piece_position, &friendly_positions);
         let expected_output: Vec<(usize, usize)> = vec![
             (3, 7),
             (0, 4),
